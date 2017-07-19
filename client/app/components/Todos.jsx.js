dc20f33f65b5017aecb23219b6452b44c412f6e7
kovaleva anastasiya <anastasiya.kovaleva@dunice.net>
2017-07-19 13:04:31 +0300

    add

---
+++ client/app/components/Todos.jsx.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+var Todos = React.createClass({
+
+    render: function() {
+        console.log('PROPS', this.props);
+        console.log('state-----', this.state);
+
+        
+        return (
+            <div>
+                <div>ewrfwerf</div>
+            </div>
+        );
+    }
+});
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # get 'todo/index'
+  # get 'static/index'
+  #
+  # resources :todos
+  #
+  # get 'todo', to: 'todo#index'
+
+
+  get 'static', to: 'static#index'
+end
