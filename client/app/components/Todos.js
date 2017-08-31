bae6f0ddb40fcdc6f2ae8707ed3e932fd64913fd
Anastasiya Kovaleva <anastasiya.kovaleva@dunice.net>
2017-08-31 14:48:55 +0300

    Fixed style

--- client/app/components/Todos.js
+++ client/app/components/Todos.js
@@ -7,6 +7,9 @@ let token = document.getElementsByName('csrf-token')[0].getAttribute('content');
 axios.defaults.headers.common['X-CSRF-Token'] = token;
 axios.defaults.headers.common['Accept'] = 'application/json';
 
+
+// const myImg = require('./assets/images/backbg.png');
+
 export default class Todos extends React.Component {
 
     // getInitialState() {
