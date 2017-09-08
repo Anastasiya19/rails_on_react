7546331a12a9ec236f848559515ba7b35fa98013
Anastasiya Kovaleva <anastasiya.kovaleva@dunice.net>
2017-09-08 16:32:11 +0300

    fix

--- config/puma.rb
+++ config/puma.rb
@@ -17,7 +17,7 @@
 
 
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 2)
 threads threads_count, threads_count
 
 preload_app!
@@ -47,7 +47,7 @@ end
 # you need to make sure to reconnect any threads in the `on_worker_boot`
 # block.
 #
-# preload_app!
+preload_app!
 
 # If you are preloading your application and using Active Record, it's
 # recommended that you close any connections to the database before workers
