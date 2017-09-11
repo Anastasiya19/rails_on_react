5767e8317be5d0be2806fbeedf956ba143921ea9
Anastasiya Kovaleva <anastasiya.kovaleva@dunice.net>
2017-09-11 16:46:27 +0300

    Add background

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'bootstrap-sass'
 
 gem 'jquery-rails'
 
+gem "responders"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.2'
 # Use postgresql as the database for Active Record
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  protect_from_forgery with: :exception
+
+  def respond_modal_with(*args, &blk)
+    options = args.extract_options!
+    options[:responder] = ModalResponder
+    respond_with *args, options, &blk
+  end
+
 end
