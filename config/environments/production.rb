b855d1508a1b6c91b9dd5256ca53a724c3d7cc60
Anastasiya Kovaleva <anastasiya.kovaleva@dunice.net>
2017-09-11 15:39:45 +0300

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,18 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      :user_name => "sugukvs92",
+      :password => "**************",
+      :authentication => "plain",
+      :enable_starttls_auto => true
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
