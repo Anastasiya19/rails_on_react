810d28ee0d4d7619bab62c307257b0c06e1ef258
Anastasiya Kovaleva <anastasiya.kovaleva@dunice.net>
2017-09-13 14:48:07 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,9 @@ Rails.application.configure do
   config.cache_classes = true
   #
   # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -85,14 +87,15 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'https://list-add-todo.herokuapp.com/' }
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
       user_name: 'dmkryhtin',
       password: 'purrweb1234',
       domain: 'localhost.com',
-      address: 'smtp.sendgrid.net',
+      address: 'smtp.gmail.com',
       port: 587,
       authentication: :plain,
       enable_starttls_auto: true
