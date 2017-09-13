50438a995e6c5d740a7fad89edbe915adb9a328c
Anastasiya Kovaleva <anastasiya.kovaleva@dunice.net>
2017-09-13 14:52:47 +0300

    fix

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       flash.now[:error] = nil
-      redirect_to new_contact_url, notice: 'Thank you for your message. We will contact you soon!'
+      redirect_to new_contact_url , notice: 'Thank you for your message. We will contact you soon!'
     else
       flash.now[:error] = 'Cannot send message'
       # render :new
